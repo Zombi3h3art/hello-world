d42999fa10bc6e1bcc9708487b28d3dc143e4129
Zombi3h3art <64819363+Zombi3h3art@users.noreply.github.com>
2025-02-06 20:01:32 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+public class HelloWorld
+{
+   public static void main(String[] args)
+   {
+      double letter = 1;
+      letter = letter + 1;
+      double x; 
+      x += 10;
+      x = 2;
+      // x = x + 10;
+      // x += 10;
+      
+      
+      
+      
+      System.out.println(" Hello World " + x);
+      // main() is a Method
+      // System. is a class
+      // .out. is a object
+      // .print & .println are Methods 
+      //  ; ends a statement
+      // the .Methods() use the System. class, .out. Object, of the Java API
+   }
+      
+}
